2bc7bef8434e75d2757dd969bf465b1d926ea5a4
sercan-works <sercantas1026@gmail.com>
2022-03-22 09:14:10 +0300

    d

--- main/settings/base.py
+++ main/settings/base.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 MEDIA_URL = "media/"
 
@@ -141,9 +141,9 @@ MEDIA_ROOT = BASE_DIR / "media_root"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#  os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+ os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Default primary key field type
